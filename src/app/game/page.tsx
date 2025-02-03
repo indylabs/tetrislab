1874f9665636d0c7fb99bbd24a8ea7641cee5828
Des McDonnell <des@indy.ie>
2025-02-03 16:19:11 +0000

    make notifications interval based (#6)
    * make notifications interval based
    
    * fixup

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,13 +1,23 @@
 "use client";
+import { useTetrisLabContext } from "@/context/TetrisLabContext";
 
+import PassiveNotification from "@/components/PassiveNotification/PassiveNotification";
+import ActionRequiringNotification from "@/components/ActionRequiringNotification/ActionRequiringNotification";
 import Tetris from "@/components/Tetris/Tetris";
+
+import { VARIANTS } from "@/constants";
 
 import styles from "./page.module.scss";
 
 export default function Game() {
+  const { state } = useTetrisLabContext();
+  const { variant } = state;
+
   return (
     <div className={styles.game}>
       <Tetris />
+      {variant === VARIANTS.A && <PassiveNotification />}
+      {variant === VARIANTS.B && <ActionRequiringNotification />}
     </div>
   );
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
-export const VARIANTS = ["control", "a", "b"];
+export enum VARIANTS {
+  Control = 'control',
+  A = 'a',
+  B = 'b'
+}
+
+export const PASSIVE_NOTIFICATION_INTERVAL = 10000; // 10 sec
+export const PASSIVE_NOTIFICATION_AUTO_HIDE_DURATION = 5000; // 5 sec
+
+export const ACTION_REQUIRING_INTERVAL = 10000; // 10 sec
