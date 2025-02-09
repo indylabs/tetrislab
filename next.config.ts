4a621f7cfdf7dcc293e3a27e660800758b6d6455
Des McDonnell <des@indy.ie>
2025-02-09 13:47:12 +0000

    added rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/thesis',
+        destination: 'https://publish.obsidian.md/des-mcdonnell/Cyberpsychology/Thesis/',
+      }
+    ]
+  },
 };
 
 export default nextConfig;
