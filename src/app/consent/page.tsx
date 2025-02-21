7412430b79321dd76d469d5a6c5df08d2ec90300
Des McDonnell <des@indy.ie>
2025-02-21 18:49:56 +0000

    fixup

--- src/app/consent/page.tsx
+++ src/app/consent/page.tsx
@@ -3,16 +3,6 @@
 import { Consent } from "@/components/Consent/Consent";
 import { Container } from "@/components/Container/Container";
 
-export const ACTION_TITLE = "Task";
-
-export const ACTION_INFO_INIT =
-  "When ready, click 'Start Task' to begin the primary task";
-export const ACTION_LABEL_INIT = "Start Task";
-
-export const ACTION_INFO_COMPLETE =
-  'Click "Continue" to proceed to the post-task steps';
-export const ACTION_LABEL_COMPLETE = "Continue";
-
 export default function ConsentPage() {
   return (
     <Container>
--- src/components/Finish/Finish.tsx
+++ src/components/Finish/Finish.tsx
@@ -64,7 +64,7 @@ export const Finish = ({ onComplete }: FinishProps) => {
     if (participantCode) {
       dispatch({ type: "ADD_PARTICIPANT_CODE", participantCode });
     }
-  }, [participantCode]);
+  }, [dispatch, participantCode]);
 
   return (
     <>
